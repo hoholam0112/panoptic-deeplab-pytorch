838a525449fe2178693ed69548d8aaa1cb10ce5a
Youngnam Kim <hoholam0112@gmail.com>
2020-06-24 13:50:31 +0900

    fixed all bug. test finished

--- post_processing/evaluation_format.py
+++ post_processing/evaluation_format.py
@@ -6,7 +6,7 @@ import torch.nn.functional as F
 from .semantic_post_processing import get_semantic_segmentation
 from .instance_post_processing import get_instance_segmentation
 
-def get_ins_list(semantic_pred,
+def get_ins_list(sem_pred,
                  center_pred,
                  offset_pred,
                  thing_list,
@@ -15,9 +15,8 @@ def get_ins_list(semantic_pred,
                  nms_kernel=7,
                  top_k=None):
     """ get instance list from prediction results.
-
     Args:
-        sementic_pred: tensor of size [1, C, H, W]. sementic segmentation prediction.
+        sem_pred: tensor of size [1, C, H, W]. sementic segmentation prediction.
         center_pred: tensor of size [1, 1, H, W]. center heatmap prediction.
         offset_pred: tensor of size [1, 2, H, W]. offset prediction.
         thing_list: List of thing class ids (int). things can be instance and belongs to foregound.
@@ -25,26 +24,22 @@ def get_ins_list(semantic_pred,
         threshold: float. threshold for center_pred activation.
         nms_kernel: int. max pooling kernel size for filtering center activations.
         top_k: int. number of center points to be preserved from predicted center heatmap.
-
     Returns:
         eval_format: list of dictionary. e.g.
             {'class_id': 13, 'mask': nparray of size (h, w) , 'score': 0.9876}
-
-
     Raises:
         AssertionError: check prediction maps' dimension.
     """
     # Check argument validity
-    assert semantic_pred.dim() == 4 and semantic_pred.size(0) == 1
+    assert sem_pred.dim() == 4 and sem_pred.size(0) == 1
     assert center_pred.dim() == 4 and center_pred.size(0) == 1
     assert offset_pred.dim() == 4 and offset_pred.size(0) == 1
 
-    sem_soft = F.softmax(semantic_pred, dim=1) # Normalize prediction scores
     # Resize prediction maps for evaluation 
     h, w = image_size
-    if h != sem_soft.size(2) or w != sem_soft.size(3):
-        sem_soft = F.interpolate(
-                sem_soft, size=(h, w),
+    if h != sem_pred.size(2) or w != sem_pred.size(3):
+        sem_pred = F.interpolate(
+                sem_pred, size=(h, w),
                 mode='bilinear', align_corners=False
             )
     if h != center_pred.size(2) or w != center_pred.size(3):
@@ -58,8 +53,8 @@ def get_ins_list(semantic_pred,
                 mode='bilinear', align_corners=False
             )
 
-    # sem_soft [1, C, H, W] -> sem_hard [1, H, W]
-    sem_hard = get_semantic_segmentation(sem_soft)
+    # sem_pred [1, C, H, W] -> sem_hard [1, H, W]
+    sem_hard = get_semantic_segmentation(sem_pred)
     ins_seg, center_points = get_instance_segmentation(
             sem_hard, center_pred, offset_pred, thing_list,
             threshold, nms_kernel, top_k)
@@ -76,13 +71,13 @@ def get_ins_list(semantic_pred,
         class_id, _ = torch.mode(sem_hard[ins_mask].view(-1, ))
         instance['class_id'] = class_id.item()
         # get polygon from binary instance mask
-        instance['mask'] = ins_mask.squeeze(0).detach().cpu().numpy()
+        instance['mask'] = ins_mask.squeeze(0).cpu().numpy()
         # Compute confidence score
-        score = torch.mean(sem_soft[:, class_id, :, :][ins_mask])
-        instance['score'] = score.item()
+        score_sum = torch.sum(sem_pred[:, class_id, :, :]*ins_mask)
+        score_mean = score_sum/torch.sum(ins_mask)
+        instance['score'] = score_mean.item()
         ins_list.append(instance)
 
     if not ins_list:
         raise RuntimeError('mage has no detected instance.')
     return ins_list
-
