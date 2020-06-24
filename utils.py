386ff8f524b8446a8c312b058d35fc3d8eec6070
Youngnam Kim <hoholam0112@gmail.com>
2020-06-24 23:48:10 +0900

    complete AverageMeter

--- utils.py
+++ utils.py
@@ -43,3 +43,29 @@ def id2rgb(id):
     r = id % 256
     return (r, g, b)
 
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count if self.count != 0 else 0
+
+
+def get_loss_info_str(loss_meter_dict):
+    msg = ''
+    for key in loss_meter_dict.keys():
+        msg += '{name}: {meter.val:.3e} ({meter.avg:.3e})\t'.format(
+            name=key, meter=loss_meter_dict[key]
+        )
+
+    return msg
